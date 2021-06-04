651f2517a9f11605b9d217484c9f03b9746ec4fe
Louis Aslett <louis.aslett@durham.ac.uk>
2021-06-04 16:35:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,15 +7,18 @@ chmod u+x $SCRIPT_DIR/config.sh
 . $SCRIPT_DIR/config.sh
 
 # Install Docker on Ubuntu 20.04
-apt update
-apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt update
-apt-cache policy docker-ce
-### Check last line shows docker and not ubuntu repo
-apt install -y docker-ce
-systemctl --no-pager status docker
+if ! command -v docker &> /dev/null
+then
+  apt update
+  apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+  apt update
+  apt-cache policy docker-ce
+  ### Check last line shows docker and not ubuntu repo
+  apt install -y docker-ce
+  systemctl --no-pager status docker
+fi
 
 # Pull images
 docker pull rocker/ml-verse:4.0.5
@@ -29,7 +32,7 @@ cp -r $SCRIPT_DIR/R /rserve
 cp -r $SCRIPT_DIR/www /rserve
 chown -R www-data:www-data /rserve
 chmod -R 755 /rserve
-echo "<?php\n\n\$DOMAIN='$DOMAIN'\n\$MAXRUNTIME='$MAXRUNTIME'\n\n?>\n" > /rserve/www/config.php
+echo -e "<?php\n\n\$DOMAIN='$DOMAIN';\n\$MAXRUNTIME='$MAXRUNTIME';\n\n?>\n" > /rserve/www/config.php
 
 echo "Starting server ..."
 /rserve/Rserve.sh > /dev/null 2>/dev/null &
