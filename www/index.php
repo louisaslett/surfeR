2aaa9c7412eb1940046aa123a5e9f978de6536fc
Louis Aslett <louis.aslett@durham.ac.uk>
2021-06-08 19:40:22 +0100

    Enable cross-site without cookies
    Enable cross-site calls without cookies.  If the DOMAIN config is empty, cross site based only on posted UUID is allowed, otherwise cookie on correct domain must be present.
    NB  Chrome will still require SSL for all pages to work if cross-site is disabled

--- www/index.php
+++ www/index.php
@@ -6,20 +6,32 @@ if(!isset($_POST['code'])) {
   echo "No R script provided.\n";
   exit();
 }
+if(!isset($_POST['uuid'])) {
+  echo "No UUID provided.\n";
+  exit();
+}
+if(strlen($_POST['uuid']) != 36) {
+  echo "Incorrect UUID length.\n";
+  exit();
+}
+$uuid = hash("sha256", $_POST['uuid']);
 
 # Establish user ID
-if(!isset($_COOKIE['id'])) {
-  $id = uniqid('', true);
-  setcookie('id', $id, array('expires' =>  time()+3600, 'domain' => $DOMAIN, 'samesite' => 'Lax'));
+if(!isset($_COOKIE['uuid'])) {
+  $id = $uuid;
+  setcookie('uuid', $id, array('expires' =>  time()+3600, 'domain' => $DOMAIN, 'samesite' => 'Lax'));
 } else {
-  setcookie('id', $_COOKIE['id'], array('expires' =>  time()+3600, 'domain' => $DOMAIN, 'samesite' => 'Lax'));
-  $id = $_COOKIE['id'];
+  setcookie('uuid', $_COOKIE['uuid'], array('expires' =>  time()+3600, 'domain' => $DOMAIN, 'samesite' => 'Lax'));
+  if($uuid != $_COOKIE['uuid']) {
+    echo "UUID mismatch between Javascript and browser.\n";
+    exit();
+  }
 }
 # Clear out any existing files
-system("rm /3/*-$id.html; rm /2/*-$id.R; rm /1/*-$id.R");
+system("rm /1/*-$uuid.html; rm /2/*-$uuid.R; rm /3/*-$uuid.R");
 
 $t = time();
-file_put_contents("/1/$t-$id.R", $_POST['code']);
-header('Location: running.php', true, 302);
+file_put_contents("/1/$t-$uuid.R", $_POST['code']);
+header('Location: running.php?uuid='.urlencode($_POST['uuid']), true, 302);
 exit();
 ?>
--- www/running.php
+++ www/running.php
@@ -1,16 +1,31 @@
 <?php
 include 'config.php';
 
-if(!isset($_COOKIE['id'])) {
-  echo "Sorry, request not recognised.  Please try again.";
+if(!isset($_GET['uuid'])) {
+  echo "No UUID provided.\n";
   exit();
 }
-setcookie('id', $_COOKIE['id'], array('expires' =>  time()+3600, 'domain' => $DOMAIN, 'samesite' => 'Lax'));
-$id = $_COOKIE['id'];
+if(strlen($_GET['uuid']) != 36) {
+  echo "Incorrect UUID length.\n";
+  exit();
+}
+$uuid = hash("sha256", $_GET['uuid']);
 
-if(count(glob("/3/*-$id.html"))>0) {
-  echo file_get_contents(glob("/3/*-$id.html")[0]);
-  system("rm /3/*-$id.html");
+if(strlen($DOMAIN) > 0) {
+  if(!isset($_COOKIE['uuid'])) {
+    echo "Sorry, cross-site request denied.";
+    exit();
+  }
+}
+if(isset($_COOKIE['uuid']) && $uuid != $_COOKIE['uuid']) {
+  echo "UUID mismatch between Javascript and browser.\n";
+  exit();
+}
+setcookie('uuid', $uuid, array('expires' =>  time()+3600, 'domain' => $DOMAIN, 'samesite' => 'Lax'));
+
+if(count(glob("/3/*-$uuid.html"))>0) {
+  echo file_get_contents(glob("/3/*-$uuid.html")[0]);
+  system("rm /3/*-$uuid.html");
   exit();
 }
 
@@ -19,18 +34,18 @@ if(array_key_exists('i', $_GET)) {
 } else {
   $i = 0;
 }
-if(count(glob("/2/*-$id.R"))>0) {
-  header("Refresh:1; url=running.php?i={$i}");
-  $runtime = time()-filemtime(glob("/2/*-$id.R")[0]);
+if(count(glob("/2/*-$uuid.R"))>0) {
+  header('Refresh:1; url=running.php?i='.urlencode($i).'&uuid='.urlencode($_GET['uuid']));
+  $runtime = time()-filemtime(glob("/2/*-$uuid.R")[0]);
   echo "Your code is currently executing!  Running for $runtime seconds ... (max allowed $MAXRUNTIME)";
   exit();
 }
-if(count(glob("/1/*-$id.R"))>0) {
-  header("Refresh:1; url=running.php?i={$i}");
+if(count(glob("/1/*-$uuid.R"))>0) {
+  header('Refresh:1; url=running.php?i='.urlencode($i).'&uuid='.urlencode($_GET['uuid']));
   $d = scandir("/1");
   array_splice($d, 0, 2);
-  $pos = array_keys(array_filter($d, fn($d2) => str_contains($d2,"-$id.R")))[0]+1;
-  $tot = count(glob("/1/*-$id.R"));
+  $pos = array_keys(array_filter($d, fn($d2) => str_contains($d2,"-$uuid.R")))[0]+1;
+  $tot = count(glob("/1/*-$uuid.R"));
   echo "Your code is in the queue waiting to run, please wait!  Currently in position $pos of $tot ... (you have been in the queue for $i seconds)";
   exit();
 }
